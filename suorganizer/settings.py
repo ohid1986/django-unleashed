29e34dd14b2deebc8c59bc50f1b752cd46abfca5
Ohid <ohid1986@users.noreply.github.com>
2016-05-05 16:39:11 +0600

    After after chapter 11

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -85,9 +85,19 @@ DATABASES = {
     }
 }
 
+# Email
+# https://docs.djangoproject.com/en/1.8/topics/email/
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SERVER_EMAIL = 'contact@django-unleashed.com'
+DEFAULT_FROM_EMAIL = 'no-reply@django-unleashed.com'
+EMAIL_SUBJECT_PREFIX = '[Startup Organizer] '
+MANAGERS = (
+    ('Us', 'ourselves@django-unleashed.com'),
+)
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -101,6 +111,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
